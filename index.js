9db3def3a62f0297ad1e4f4ad958ddba047f2988
AbelTarazona <abel.tarazona@urp.edu.pe>
2019-08-28 00:58:56 -0500

    Se agrego nuevas emociones y mejor UI

--- index.js
+++ index.js
@@ -1,64 +1,77 @@
-/*
-- 50   Emocionado       2
-- 150  Alegre           1
--- 250 Normal           0
-- 350  Triste           -1
-- 450  Desesperado      -2
+/*    +/- 30
+-   160      Emocionado       7
+-   190      Alegre           6
+--  220      Normal           5
+--- 250
+-   280      Pasable          4
+-   310      Mmm              3
+-   340      Lo odio          2
 */
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+var emotion = "";
+
 ctx.strokeStyle = "#448ee1";
 ctx.lineWidth = 5;
 var x_inicial = 50;
 var y_inicial;
 ctx.beginPath();
 ctx.moveTo(x_inicial, 250);
-//ctx.stroke();
 
 function addEmotion() {
-    var emotion = $("#sltEmojis").val();
+
+    if (emotion == "") {alert('Seleccione una emocion.'); return;}
+
     var emotion_def = "";
     ctx.font = "30px Arial";
     switch (emotion) {
-        case '2':
+        case 7:
             emotion_def = 'Emocionado';
             x_inicial += 50;
-            y_inicial = 50;
+            y_inicial = 160;
             ctx.lineTo(x_inicial, y_inicial);
             ctx.arc(x_inicial, y_inicial, 5, 0, 2 * Math.PI, false);
-            ctx.fillText("😍", x_inicial, y_inicial - 5);
+            ctx.fillText("😄", x_inicial, y_inicial - 5);
             break;
-        case '1':
+        case 6:
             emotion_def = 'Alegre';
             x_inicial += 50;
-            y_inicial = 150;
+            y_inicial = 190;
             ctx.lineTo(x_inicial, y_inicial);
             ctx.arc(x_inicial, y_inicial, 5, 0, 2 * Math.PI, false);
-            ctx.fillText("😀", x_inicial, y_inicial - 5);
+            ctx.fillText("😃", x_inicial, y_inicial - 5);
             break;
-        case '0':
+        case 5:
             emotion_def = 'Normal';
             x_inicial += 50;
-            y_inicial = 250;
+            y_inicial = 220;
             ctx.lineTo(x_inicial, y_inicial);
             ctx.arc(x_inicial, y_inicial, 5, 0, 2 * Math.PI, false);
-            ctx.fillText("😬", x_inicial, y_inicial - 5);
+            ctx.fillText("🙂", x_inicial, y_inicial - 5);
             break;
-        case '-1':
+        case 4:
             emotion_def = 'Triste';
             x_inicial += 50;
-            y_inicial = 350;
+            y_inicial = 280;
             ctx.lineTo(x_inicial, y_inicial);
             ctx.arc(x_inicial, y_inicial, 5, 0, 2 * Math.PI, false);
             ctx.fillText("😐", x_inicial, y_inicial - 5);
             break;
-        case '-2':
+        case 3:
             emotion_def = 'Desesperado';
             x_inicial += 50;
-            y_inicial = 450;
+            y_inicial = 310;
             ctx.lineTo(x_inicial, y_inicial);
             ctx.arc(x_inicial, y_inicial, 5, 0, 2 * Math.PI, false);
             ctx.fillText("😕", x_inicial, y_inicial - 5);
+            break;
+        case 2:
+            emotion_def = 'Desesperado';
+            x_inicial += 50;
+            y_inicial = 340;
+            ctx.lineTo(x_inicial, y_inicial);
+            ctx.arc(x_inicial, y_inicial, 5, 0, 2 * Math.PI, false);
+            ctx.fillText("😡", x_inicial, y_inicial - 5);
             break;
     }
     ctx.stroke();
@@ -66,6 +79,11 @@ function addEmotion() {
     //alert(emotion_def);
 }
 
+//https://stackoverflow.com/questions/14488849/higher-dpi-graphics-with-html5-ca
+// nvas
+// https://stackoverflow.com/questions/279749/detecting-the-system-dpi-ppi-from-j
+// s-css
 
-//https://stackoverflow.com/questions/14488849/higher-dpi-graphics-with-html5-canvas
-//https://stackoverflow.com/questions/279749/detecting-the-system-dpi-ppi-from-js-css
+function alerta(emotionFromView) {
+    emotion = emotionFromView;
+}
